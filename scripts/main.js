1b2be25e50869c29ae7ffdd96ebda7281efb621d
mobyvb <mobyvb@gmail.com>
2013-08-24 03:55:16 -0400

    improve collision detection (not perfected yet)

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,14 @@ var fps;
 var ctx;
 var width;
 var height;
-var player = {x:0, y:0, width:25, height:50, vx:0, vy:0, ax:10000, jumpPower:800};
+var player = {x:0, y:0, width:100, height:10c0, vx:0, vy:0, ax:10000, jumpPower:1000, airborne:false};
 var rectangle = {x:500, y:400, width:100, height:100};
 var TWO_PI = 2*Math.PI;
 var keysDown = {};
 var friction = 0.8;
 var gravity = 2000;
+var newX;
+var newY;
 
 setup();
 start();
@@ -44,33 +46,32 @@ function update(dt) {
   fps = ~~(1000/dt);
   var elapsedSeconds = dt/1000;
   if (38 in keysDown) { // up
-    if(!player.bottomFree) {
+    if(!player.airborne) {
       player.vy = -player.jumpPower;
-      player.bottomFree = true;
+      player.airborne = true;
     }
   }
   if (37 in keysDown) { // left
     player.vx -= player.ax*elapsedSeconds;
-    player.rightFree = true;
   }
   if (39 in keysDown) { // right
     player.vx += player.ax*elapsedSeconds;
-    player.leftFree = true;
   }
-  player.vy += gravity*elapsedSeconds;
-  player.y += player.vy*elapsedSeconds;
+
   player.vx *= friction;
+  player.vy += gravity*elapsedSeconds;
+
+  var willCollide = handleCollision(rectangle, player);
   player.x += player.vx*elapsedSeconds;
+  player.y += player.vy*elapsedSeconds;
 
   if (player.x < 0) player.x = 0;
   else if (player.x+player.width > width) player.x = width-player.width;
   if(player.y+player.height > height) {
     player.y = height-player.height;
     player.vy = 0;
-    player.bottomFree = false;
+    player.airborne = false;
   }
-
-  handleCollision(rectangle, player);
 }
 
 function draw() {
@@ -100,38 +101,42 @@ function draw() {
 }
 
 function handleCollision(staticObj, dynamicObj) {
+  var willCollide = {l:false, r:false, t:false, b:false};
+
   var minXOffset = staticObj.width/2 + dynamicObj.width/2;
   var minYOffset = staticObj.height/2 + dynamicObj.height/2;
 
-  var xOffset = staticObj.x+staticObj.width/2 - dynamicObj.x+dynamicObj.width/2;
-  var yOffset = staticObj.y+staticObj.height/2 - dynamicObj.y+dynamicObj.height/2;
+  var xOffset = (staticObj.x+staticObj.width/2) - (dynamicObj.x+dynamicObj.width/2);
+  var yOffset = (staticObj.y+staticObj.height/2) - (dynamicObj.y+dynamicObj.height/2);
 
-  if (Math.abs(xOffset) < minXOffset && Math.abs(yOffset) < minYOffset) { // collision
+  if (Math.abs(xOffset) <= minXOffset && Math.abs(yOffset) <= minYOffset) { // collision
     if (Math.abs(xOffset) < Math.abs(yOffset)) { // collision on top or bottom
       if (yOffset > 0) { // collision on top
-        console.log('top');
         dynamicObj.y = staticObj.y-dynamicObj.height;
         dynamicObj.vy = 0;
+        player.airborne = false;
+        willCollide.t = true;
       }
       else { // collision on bottom
-        console.log('bottom');
         dynamicObj.y = staticObj.y+staticObj.height;
         dynamicObj.vy = 0;
+        willCollide.b = true;
       }
     }
     else { // collision on left or right
       if (xOffset > 0) { // collision on left
-        console.log('left');
         dynamicObj.x = staticObj.x-dynamicObj.width;
-        dynamicObj.vx = 0;
+        if (dynamicObj.vx > 0) dynamicObj.vx = 0;
+        willCollide.l = true;
       }
       else { // collision on right
-        console.log('right');
         dynamicObj.x = staticObj.x+staticObj.width;
-        dynamicObj.vx = 0;
+        if (dynamicObj.vx < 0) dynamicObj.vx = 0;
+        willCollide.r = true;
       }
     }
   }
+  return willCollide;
 }
 
 addEventListener("keydown", function (e) {

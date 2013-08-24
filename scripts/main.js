6689c5fee77ec6ba73624d31555b500f9a7a9f9e
mobyvb <mobyvb@gmail.com>
2013-08-24 02:37:27 -0400

    buggy collision detection

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,8 @@ var fps;
 var ctx;
 var width;
 var height;
-var player = {x:0, y:0, width:25, height:50, vx:0, vy:0, ax:10000, jumpPower:800, airborne:false};
-var rectangle = {x:500, y:250, width:100, height:100};
+var player = {x:0, y:0, width:25, height:50, vx:0, vy:0, ax:10000, jumpPower:800};
+var rectangle = {x:500, y:400, width:100, height:100};
 var TWO_PI = 2*Math.PI;
 var keysDown = {};
 var friction = 0.8;
@@ -44,16 +44,18 @@ function update(dt) {
   fps = ~~(1000/dt);
   var elapsedSeconds = dt/1000;
   if (38 in keysDown) { // up
-    if(!player.airborne) {
+    if(!player.bottomFree) {
       player.vy = -player.jumpPower;
-      player.airborne = true;
+      player.bottomFree = true;
     }
   }
   if (37 in keysDown) { // left
     player.vx -= player.ax*elapsedSeconds;
+    player.rightFree = true;
   }
   if (39 in keysDown) { // right
     player.vx += player.ax*elapsedSeconds;
+    player.leftFree = true;
   }
   player.vy += gravity*elapsedSeconds;
   player.y += player.vy*elapsedSeconds;
@@ -65,8 +67,10 @@ function update(dt) {
   if(player.y+player.height > height) {
     player.y = height-player.height;
     player.vy = 0;
-    player.airborne = false;
+    player.bottomFree = false;
   }
+
+  handleCollision(rectangle, player);
 }
 
 function draw() {
@@ -93,6 +97,41 @@ function draw() {
   ctx.moveTo(width, 0);
   ctx.lineTo(0, 0);
   ctx.stroke();
+}
+
+function handleCollision(staticObj, dynamicObj) {
+  var minXOffset = staticObj.width/2 + dynamicObj.width/2;
+  var minYOffset = staticObj.height/2 + dynamicObj.height/2;
+
+  var xOffset = staticObj.x+staticObj.width/2 - dynamicObj.x+dynamicObj.width/2;
+  var yOffset = staticObj.y+staticObj.height/2 - dynamicObj.y+dynamicObj.height/2;
+
+  if (Math.abs(xOffset) < minXOffset && Math.abs(yOffset) < minYOffset) { // collision
+    if (Math.abs(xOffset) < Math.abs(yOffset)) { // collision on top or bottom
+      if (yOffset > 0) { // collision on top
+        console.log('top');
+        dynamicObj.y = staticObj.y-dynamicObj.height;
+        dynamicObj.vy = 0;
+      }
+      else { // collision on bottom
+        console.log('bottom');
+        dynamicObj.y = staticObj.y+staticObj.height;
+        dynamicObj.vy = 0;
+      }
+    }
+    else { // collision on left or right
+      if (xOffset > 0) { // collision on left
+        console.log('left');
+        dynamicObj.x = staticObj.x-dynamicObj.width;
+        dynamicObj.vx = 0;
+      }
+      else { // collision on right
+        console.log('right');
+        dynamicObj.x = staticObj.x+staticObj.width;
+        dynamicObj.vx = 0;
+      }
+    }
+  }
 }
 
 addEventListener("keydown", function (e) {
